84b1043cfd3f2f84882a10ba61f35a33e15efd20
Wellington Costa <wellington.costa128@gmail.com>
2017-12-26 16:07:40 -0300

    Métodos base

--- app/src/main/java/br/com/wellingtoncosta/githubusers/ui/base/BaseActivity.java
+++ app/src/main/java/br/com/wellingtoncosta/githubusers/ui/base/BaseActivity.java
@@ -2,9 +2,12 @@ package br.com.wellingtoncosta.githubusers.ui.base;
 
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import javax.inject.Inject;
 
@@ -31,7 +34,7 @@ public abstract class BaseActivity<T extends ViewModel> extends DaggerAppCompatA
 
     public abstract void setupViewModel();
 
-    public void setupToolbar(Toolbar toolbar,int stringTitleResId, boolean showBackButton) {
+    public void setupToolbar(Toolbar toolbar, int stringTitleResId, boolean showBackButton) {
         if (toolbar != null) {
             setSupportActionBar(toolbar);
             ActionBar actionBar = getSupportActionBar();
@@ -44,6 +47,14 @@ public abstract class BaseActivity<T extends ViewModel> extends DaggerAppCompatA
                 }
             }
         }
+    }
+
+    public void showLongSnackbar(View view, String text) {
+        Snackbar.make(view, text, Snackbar.LENGTH_LONG).show();
+    }
+
+    public void showLongSnackbar(View view, int stringResId) {
+        Snackbar.make(view, getString(stringResId), Snackbar.LENGTH_LONG).show();
     }
 
     public ViewModelProvider.Factory getViewModelFactory() {
