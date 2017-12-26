80d1a94c5f9597438d6e34c9510e8c5dd97e418e
Wellington Costa <wellington.costa128@gmail.com>
2017-12-26 12:07:49 -0300

    imports removidos

--- app/src/main/java/br/com/wellingtoncosta/githubusers/data/remote/response/Response.java
+++ app/src/main/java/br/com/wellingtoncosta/githubusers/data/remote/response/Response.java
@@ -1,8 +1,5 @@
 package br.com.wellingtoncosta.githubusers.data.remote.response;
 
-import java.util.Collections;
-import java.util.List;
-
 import io.reactivex.annotations.NonNull;
 
 import static br.com.wellingtoncosta.githubusers.data.remote.response.Status.ERROR;
@@ -17,19 +14,19 @@ public class Response<T> {
     public final Status status;
 
     @NonNull
-    public final List<T> data;
+    public final T data;
 
-    private Response(@NonNull Status status, @NonNull List<T> data) {
+    private Response(@NonNull Status status, @NonNull T data) {
         this.status = status;
         this.data = data;
     }
 
-    public static <T> Response<T> success(List<T> data) {
+    public static <T> Response<T> success(T data) {
         return new Response<>(SUCCESS, data);
     }
 
     public static <T> Response<T> error() {
-        return new Response<>(ERROR, Collections.emptyList());
+        return new Response<>(ERROR, null);
     }
 
 }
