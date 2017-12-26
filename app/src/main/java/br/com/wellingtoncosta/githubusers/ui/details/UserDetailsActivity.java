a7744dcf8eea6a17fe885f1c4863fb138ae97c42
Wellington Costa <wellington.costa128@gmail.com>
2017-12-26 16:09:03 -0300

    Estrutura inicial da tela de detalhes do usuário

---
+++ app/src/main/java/br/com/wellingtoncosta/githubusers/ui/details/UserDetailsActivity.java
@@ -0,0 +1,15 @@
+package br.com.wellingtoncosta.githubusers.ui.details;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.wellingtoncosta.githubusers.R;
+
+public class UserDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+    }
+}
