f6aa93feb77c6bc1eacf3aeb22af93694091705c
Wellington Costa <wellington.costa128@gmail.com>
2017-12-26 12:08:19 -0300

    Estrutura de ViewModels

---
+++ app/src/main/java/br/com/wellingtoncosta/githubusers/di/modules/ViewModelModule.java
@@ -0,0 +1,27 @@
+package br.com.wellingtoncosta.githubusers.di.modules;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+import br.com.wellingtoncosta.githubusers.di.keys.ViewModelKey;
+import br.com.wellingtoncosta.githubusers.ui.ViewModelFactory;
+import br.com.wellingtoncosta.githubusers.ui.search.SearchUsersViewModel;
+import dagger.Binds;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+
+/**
+ * @author Wellington Costa on 26/12/2017.
+ */
+@Module
+public interface ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(SearchUsersViewModel.class)
+    ViewModel bindSearchUsersViewModel(SearchUsersViewModel searchUsersViewModel);
+
+    @Binds
+    ViewModelProvider.Factory bindViewModelFactory(ViewModelFactory factory);
+
+}
---
+++ app/src/main/java/br/com/wellingtoncosta/githubusers/domain/repository/RepoRepository.java
@@ -0,0 +1,10 @@
+package br.com.wellingtoncosta.githubusers.domain;
+
+/**
+ * @author Wellington Costa on 26/12/2017.
+ */
+public class RepoRepository {
+
+
+
+}
