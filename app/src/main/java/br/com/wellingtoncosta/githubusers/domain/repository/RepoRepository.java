27b229ed2768f69aa1861a3edddbe8af9dc92dfb
Wellington Costa <wellington.costa128@gmail.com>
2017-12-26 12:10:36 -0300

    Repositórios

--- app/src/main/java/br/com/wellingtoncosta/githubusers/domain/repository/RepoRepository.java
+++ app/src/main/java/br/com/wellingtoncosta/githubusers/domain/repository/RepoRepository.java
@@ -1,10 +1,31 @@
-package br.com.wellingtoncosta.githubusers.domain;
+package br.com.wellingtoncosta.githubusers.domain.repository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import br.com.wellingtoncosta.githubusers.data.remote.GitHubApi;
+import br.com.wellingtoncosta.githubusers.domain.model.Repo;
+import io.reactivex.Observable;
 
 /**
  * @author Wellington Costa on 26/12/2017.
  */
 public class RepoRepository {
 
+    private GitHubApi api;
 
+    @Inject
+    public RepoRepository(GitHubApi api) {
+        this.api = api;
+    }
+
+    public Observable<List<Repo>> getRepos(String username) {
+        return api.getRepos(username);
+    }
+
+    public Observable<List<Repo>> getStarredRepos(String username) {
+        return api.getStarredRepos(username);
+    }
 
 }
--- app/src/main/java/br/com/wellingtoncosta/githubusers/domain/repository/UserRepository.java
+++ app/src/main/java/br/com/wellingtoncosta/githubusers/domain/repository/UserRepository.java
@@ -1,10 +1,39 @@
-package br.com.wellingtoncosta.githubusers.domain;
+package br.com.wellingtoncosta.githubusers.domain.repository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import br.com.wellingtoncosta.githubusers.data.remote.GitHubApi;
+import br.com.wellingtoncosta.githubusers.domain.model.User;
+import io.reactivex.Observable;
 
 /**
  * @author Wellington Costa on 26/12/2017.
  */
 public class UserRepository {
 
+    private GitHubApi api;
 
+    @Inject
+    public UserRepository(GitHubApi api) {
+        this.api = api;
+    }
+
+    public Observable<List<User>> getUsers() {
+        return api.getUsers();
+    }
+
+    public Observable<User> getUser(String username) {
+        return api.getUser(username);
+    }
+
+    public Observable<List<User>> getFollowers(String username) {
+        return api.getFollowers(username);
+    }
+
+    public Observable<List<User>> getFollowing(String username) {
+        return api.getFollowing(username);
+    }
 
 }
