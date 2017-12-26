558ea48b543460454f3a7db0aa8a143a48f930f0
Wellington Costa <wellington.costa128@gmail.com>
2017-12-26 11:15:32 -0300

    Estrutura inicial da tela de pesquisa de usuários

---
+++ app/src/main/java/br/com/wellingtoncosta/githubusers/ui/search/SearchActivity.java
@@ -0,0 +1,16 @@
+package br.com.wellingtoncosta.githubusers.ui.search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.wellingtoncosta.githubusers.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+}
